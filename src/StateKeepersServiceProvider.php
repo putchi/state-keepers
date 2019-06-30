9898d346c3a75f5c4eefded3f392f6233c0e674f
putchi <alex.r@routeperfect.com>
2019-06-30 11:25:28 +0300

    added Lumen support

--- src/StateKeepersServiceProvider.php
+++ src/StateKeepersServiceProvider.php
@@ -7,31 +7,44 @@ use Illuminate\Support\ServiceProvider;
 class StateKeepersServiceProvider extends ServiceProvider {
 
     /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        if ($this->isLumen()) {
+            require_once 'lumen.php';
+        }
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/state.php', 'state'
+        );
+    }
+
+    /**
      * Perform post-registration booting of services.
      *
      * @return void
      */
     public function boot() {
-        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'state');
+        $this->loadTranslationsFrom(__DIR__ . '/resources/lang', 'state');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/config/state.php' => config_path('state.php'),
             ], 'config');
             $this->publishes([
-                __DIR__.'/resources/lang' => resource_path('lang'),
+                __DIR__ . '/resources/lang' => resource_path('lang'),
             ], 'resources');
         }
     }
 
     /**
-     * Register the service provider.
+     * Check if package is running under Lumen app
      *
-     * @return void
+     * @return bool
      */
-    public function register() {
-        $this->mergeConfigFrom(
-            __DIR__.'/config/state.php', 'state'
-        );
+    protected function isLumen() {
+        return str_contains($this->app->version(), 'Lumen');
     }
 }
